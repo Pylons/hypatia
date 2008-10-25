248645614ed9044a8af72cd6dadb9b62d4bbe531
Chris McDonough <chrism@plope.com>
2008-10-25 15:55:04 +0000

    Naive facet index implementation.

---
+++ repoze/catalog/indexes/facet.py
@@ -0,0 +1,101 @@
+from persistent import Persistent
+from zope.interface import implements
+
+from repoze.catalog.indexes.keyword import KeywordIndex
+from repoze.catalog.interfaces import ICatalogIndex
+from repoze.catalog.indexes.common import CatalogIndex
+
+_marker = ()
+
+class FacetIndex(CatalogIndex, KeywordIndex):
+    implements(ICatalogIndex)
+
+    def __init__(self, discriminator, taxonomy, family=None):
+        if not callable(discriminator):
+            if not isinstance(discriminator, basestring):
+                raise ValueError('discriminator value must be callable or a '
+                                 'string')
+        self.discriminator = discriminator
+        if family is not None:
+            self.family = family
+        self.taxonomy = self.family.OO.Set(taxonomy)
+        self.clear()
+
+    def index_doc(self, docid, object):
+        """ Pass in an integer document id and an object supporting a
+        sequence of facet specifiers ala ['style:gucci:handbag'] via
+        the discriminator"""
+
+        if callable(self.discriminator):
+            value = self.discriminator(object, _marker)
+        else:
+            value = getattr(object, self.discriminator, _marker)
+
+        if value is _marker:
+            # unindex the previous value
+            self.unindex_doc(docid)
+            return None
+
+        if isinstance(value, Persistent):
+            raise ValueError('Catalog cannot index persistent object %s' %
+                             value)
+
+        old = self._rev_index.get(docid)
+        if old is not None:
+            self.unindex_doc(docid)
+
+        changed = False
+
+        for facet in value:
+            L = []
+            categories = facet.split(':')
+            for category in categories:
+                L.append(category)
+                facet_candidate = ':'.join(L)
+                for fac in self.taxonomy:
+                    if facet_candidate == fac:
+                        changed = True
+                        fwset = self._fwd_index.get(facet_candidate)
+                        if fwset is None:
+                            fwset = self.family.II.Set()
+                            self._fwd_index[fac] = fwset
+                        fwset.insert(docid)
+                        revset = self._rev_index.get(docid)
+                        if revset is None:
+                            revset = self.family.OO.Set()
+                            self._rev_index[docid] = revset
+                        revset.insert(fac)
+        
+        if changed:
+            self._num_docs.change(1)
+
+    def counts(self, docids, omit_facets=()):
+        """ Given a set of docids (usually returned from query),
+        provide count information for further facet narrowing.
+        Optionally omit count information for facets and their
+        ancestors that are in 'omit_facets' (a sequence of facets)"""
+
+        effective_omits = self.family.OO.Set()
+
+        for omit_facet in omit_facets:
+            L = []
+            categories = omit_facet.split(':')
+            for category in categories:
+                L.append(category)
+                effective_omits.insert(':'.join(L))
+
+        include_facets = self.family.OO.difference(self.taxonomy,
+                                                   effective_omits)
+
+        counts = {}
+        
+        for docid in docids:
+            available_facets = self._rev_index.get(docid)
+            appropriate_facets = self.family.OO.intersection(
+                include_facets, available_facets)
+            for facet in appropriate_facets:
+                count = counts.get(facet, 0)
+                count += 1
+                counts[facet] = count
+
+        return counts
---
+++ repoze/catalog/indexes/tests/test_facet.py
@@ -0,0 +1,125 @@
+import unittest
+
+taxonomy = [
+    'price',
+    'price:0-100',
+    'price:100-500',
+    'price:100-*',
+    'color',
+    'color:blue',
+    'color:red',
+    'size',
+    'size:small',
+    'size:medium',
+    'size:large',
+    'style',
+    'style:gucci',
+    'style:gucci:handbag',
+    'style:gucci:dress',
+    ]
+
+class TestFacetIndex(unittest.TestCase):
+    def _getTargetClass(self):
+        from repoze.catalog.indexes.facet import FacetIndex
+        return FacetIndex
+
+    def _makeOne(self, taxonomy):
+        klass = self._getTargetClass()
+        return klass(lambda x, default: x, taxonomy)
+    
+
+    def _populateIndex(self, idx):
+        idx.index_doc(1, ['price:0-100', 'color:blue', 'style:gucci:handbag'])
+        idx.index_doc(2, ['price:0-100', 'color:blue', 'style:gucci:dress'])
+        idx.index_doc(3, ['price:0-100', 'color:red', 'color:blue',
+                          'style:gucci'])
+        idx.index_doc(4, ['size:large'])
+
+    def test_search(self):
+        index = self._makeOne(taxonomy)
+        self._populateIndex(index)
+
+        result = index.search(['color:blue', 'color:red'])
+        self.assertEqual(sorted(list(result)), [3])
+        result = index.search(['price'])
+        self.assertEqual(sorted(list(result)), [1,2,3])
+        result = index.search(['price:0-100'])
+        self.assertEqual(sorted(list(result)), [1,2,3])
+        result = index.search(['style:gucci'])
+        self.assertEqual(sorted(list(result)), [1,2,3])
+        result = index.search(['style:gucci:handbag'])
+        self.assertEqual(sorted(list(result)), [1])
+        result = index.search(['size'])
+        self.assertEqual(sorted(list(result)), [4])
+        result = index.search(['size:large'])
+        self.assertEqual(sorted(list(result)), [4])
+        result = index.search(['size:nonexistent'])
+        self.assertEqual(sorted(list(result)), [])
+        result = index.search(['nonexistent'])
+        self.assertEqual(sorted(list(result)), [])
+
+        index.unindex_doc(1)
+        result = index.search(['price'])
+        self.assertEqual(sorted(list(result)), [2,3])
+        result = index.search(['price:0-100'])
+        self.assertEqual(sorted(list(result)), [2,3])
+        result = index.search(['style:gucci'])
+        self.assertEqual(sorted(list(result)), [2,3])
+        result = index.search(['style:gucci:handbag'])
+        self.assertEqual(sorted(list(result)), [])
+        
+        index.unindex_doc(2)
+        result = index.search(['price'])
+        self.assertEqual(sorted(list(result)), [3])
+        result = index.search(['price:0-100'])
+        self.assertEqual(sorted(list(result)), [3])
+        result = index.search(['style:gucci'])
+        self.assertEqual(sorted(list(result)), [3])
+        
+        index.unindex_doc(4)
+        result = index.search(['size'])
+        self.assertEqual(sorted(list(result)), [])
+        result = index.search(['size:large'])
+        self.assertEqual(sorted(list(result)), [])
+        result = index.search(['size:nonexistent'])
+        self.assertEqual(sorted(list(result)), [])
+        result = index.search(['nonexistent'])
+        self.assertEqual(sorted(list(result)), [])
+
+    def test_counts(self):
+        index = self._makeOne(taxonomy)
+        self._populateIndex(index)
+
+        facets = ['price:0-100']
+        result = index.search(facets)
+        self.assertEqual(sorted(list(result)), [1,2,3])
+        counts = index.counts(result, facets)
+        self.assertEqual(counts['style'], 3)
+        self.assertEqual(counts['style:gucci'], 3)
+        self.assertEqual(counts['style:gucci:handbag'], 1)
+        self.assertEqual(counts['style:gucci:dress'], 1)
+        self.assertEqual(counts['color'], 3)
+        self.assertEqual(counts['color:blue'], 3)
+        self.assertEqual(counts['color:red'], 1)
+        self.assertEqual(len(counts), 7)
+
+        facets = ['price:0-100', 'color:red']
+        result = index.search(facets)
+        self.assertEqual(sorted(list(result)), [3])
+        counts = index.counts(result, facets)
+        self.assertEqual(counts['style'], 1)
+        self.assertEqual(counts['style:gucci'], 1)
+        self.assertEqual(counts['color:blue'], 1)
+        self.assertEqual(len(counts), 3)
+
+        facets = ['size:large']
+        result = index.search(facets)
+        self.assertEqual(sorted(list(result)), [4])
+        counts = index.counts(result, facets)
+        self.assertEqual(counts, {})
+
+        facets = ['size']
+        result = index.search(facets)
+        self.assertEqual(sorted(list(result)), [4])
+        counts = index.counts(result, facets)
+        self.assertEqual(counts, {'size:large':1})
