dd791e2fb62ad24229e803b60f1f3421e4f8145a
Tres Seaver <tseaver@palladion.com>
2008-11-10 22:14:29 +0000

    De-clever-ify the tests (they still test irrelevant implementation details).

--- repoze/catalog/indexes/tests/test_path.py
+++ repoze/catalog/indexes/tests/test_path.py
@@ -17,168 +17,310 @@
 import unittest
 
 class PathIndexTests(unittest.TestCase):
-    """ Test PathIndex objects """
-
-    def setUp(self):
+    """ Test PathIndex objects
+    """
+    def _getTargetClass(self):
         from repoze.catalog.indexes.path import CatalogPathIndex
-        self._index = CatalogPathIndex('path')
-        self._values = {
-          1 : Dummy("/aa/aa/aa/1.html"),
-          2 : Dummy("/aa/aa/bb/2.html"),
-          3 : Dummy("/aa/aa/cc/3.html"),
-          4 : Dummy("/aa/bb/aa/4.html"),
-          5 : Dummy("/aa/bb/bb/5.html"),
-          6 : Dummy("/aa/bb/cc/6.html"),
-          7 : Dummy("/aa/cc/aa/7.html"),
-          8 : Dummy("/aa/cc/bb/8.html"),
-          9 : Dummy("/aa/cc/cc/9.html"),
-          10 : Dummy("/bb/aa/aa/10.html"),
-          11 : Dummy("/bb/aa/bb/11.html"),
-          12 : Dummy("/bb/aa/cc/12.html"),
-          13 : Dummy("/bb/bb/aa/13.html"),
-          14 : Dummy("/bb/bb/bb/14.html"),
-          15 : Dummy("/bb/bb/cc/15.html"),
-          16 : Dummy("/bb/cc/aa/16.html"),
-          17 : Dummy("/bb/cc/bb/17.html"),
-          18 : Dummy("/bb/cc/cc/18.html")
-        }
+        return CatalogPathIndex
 
-    def _populateIndex(self):
-        for k, v in self._values.items():
-            self._index.index_doc( k, v )
+    def _makeOne(self, values=None):
+        if values is None:
+            values = {}
+        index = self._getTargetClass()('path')
+        for doc_id, path in values.items():
+            index.index_doc(doc_id, path)
+        return index
 
-    def test_interfaces(self):
-        from repoze.catalog.interfaces import ICatalogIndex
+    def test_class_conforms_to_ICatalogIndex(self):
         from zope.interface.verify import verifyClass
-        from repoze.catalog.indexes.path import CatalogPathIndex
-        verifyClass(ICatalogIndex, CatalogPathIndex)
+        from repoze.catalog.interfaces import ICatalogIndex
+        verifyClass(ICatalogIndex, self._getTargetClass())
 
-    def testEmpty(self):
-        self.assertEqual(self._index.numObjects() ,0)
-        self.assertEqual(self._index.getEntryForObject(1234), None)
-        self._index.unindex_doc( 1234 ) # nothrow
-        result = self._index.apply({"suxpath":"xxx"})
+    def test_empty_index(self):
+        index = self._makeOne({})
+        self.assertEqual(index.numObjects() ,0)
+        self.assertEqual(index.getEntryForObject(1234), None)
+        index.unindex_doc(1234) # nothrow
+        result = index.apply({"suxpath":"xxx"})
         self.assertEqual(list(result), [])
 
-    def testUnIndex(self):
-        self._populateIndex()
-        self.assertEqual(self._index.numObjects(), 18)
+    def test_nonempty_index(self):
+        index = self._makeOne(VALUES)
+        self.assertEqual(index.numObjects(), 18)
 
-        for k in self._values.keys():
-            self._index.unindex_doc(k)
+    def test_unindex_doc(self):
+        index = self._makeOne(VALUES)
 
-        self.assertEqual(self._index.numObjects(), 0)
-        self.assertEqual(len(self._index._index), 0)
-        self.assertEqual(len(self._index._unindex), 0)
+        for doc_id in VALUES.keys():
+            index.unindex_doc(doc_id)
 
-    def testReindex(self):
-        self._populateIndex()
-        self.assertEqual(self._index.numObjects(), 18)
+        self.assertEqual(index.numObjects(), 0)
+        self.assertEqual(len(index._index), 0)
+        self.assertEqual(len(index._unindex), 0)
 
+    def test_reindex_doesnt_dupe(self):
+        index = self._makeOne()
         o = Dummy('/foo/bar')
-        self._index.index_doc(19, o)
-        self.assertEqual(self._index.numObjects(), 19)
-        self._index.index_doc(19, o)
-        self.assertEqual(self._index.numObjects(), 19)
+        index.index_doc(1, o)
+        self.assertEqual(index.numObjects(), 1)
+        index.index_doc(1, o)
+        self.assertEqual(index.numObjects(), 1)
 
-    def testUnIndexError(self):
-        self._populateIndex()
+    def test_unindex_nomatch_doesnt_raise(self):
+        index = self._makeOne()
         # this should not raise an error
-        self._index.unindex_doc(-1)
+        index.unindex_doc(-1)
 
         # nor should this
-        self._index._unindex[1] = "/broken/thing"
-        self._index.unindex_doc(1)
+        index._unindex[1] = "/broken/thing"
+        index.unindex_doc(1)
 
-    def testRoot(self):
+    def test_searches_against_root_wo_level(self):
+        index = self._makeOne(VALUES)
+        expected = range(1,19)
 
-        self._populateIndex()
-        tests = ( ("/", 0, range(1,19)), )
+        results = list(index.apply({'query': '/'}).keys())
+        self.assertEqual(results, expected)
 
-        for comp, level, results in tests:
-            for path in [comp,"/"+comp,"/"+comp+"/"]:
-                res = self._index.apply({"query":path,"level":level})
-                lst = list(res.keys())
-                self.assertEqual(lst,results)
+    def test_searches_against_root_w_level(self):
+        index = self._makeOne(VALUES)
+        comp = "/"
+        level = 0
+        expected = range(1,19)
 
-        for comp,level,results in tests:
-            for path in [comp,"/"+comp,"/"+comp+"/"]:
-                res = self._index.apply({"query":((path,level), )})
-                lst = list(res.keys())
-                self.assertEqual(lst,results)
+        results = list(index.apply({'query': '/', 'level': 0}).keys())
+        self.assertEqual(results, expected)
 
-    def testSimpleTests(self):
+    def test_root_aa_wo_level(self):
+        index = self._makeOne(VALUES)
+        expected = [1,2,3,4,5,6,7,8,9]
 
-        self._populateIndex()
-        tests = [
-            ("aa", 0, [1,2,3,4,5,6,7,8,9]),
-            ("aa", 1, [1,2,3,10,11,12] ),
-            ("bb", 0, [10,11,12,13,14,15,16,17,18]),
-            ("bb", 1, [4,5,6,13,14,15] ),
-            ("bb/cc", 0, [16,17,18] ),
-            ("bb/cc", 1, [6,15] ),
-            ("bb/aa", 0, [10,11,12] ),
-            ("bb/aa", 1, [4,13] ),
-            ("aa/cc", -1, [3,7,8,9,12] ),
-            ("bb/bb", -1, [5,13,14,15] ),
-            ("18.html", 3, [18] ),
-            ("18.html", -1, [18] ),
-            ("cc/18.html", -1, [18] ),
-            ("cc/18.html", 2, [18] ),
-        ]
+        results = list(index.apply({'query': '/aa'}).keys())
+        self.assertEqual(results, expected)
 
-        for comp,level,results in tests:
-            for path in [comp,"/"+comp,"/"+comp+"/"]:
-                res = self._index.apply({'query':path,"level":level})
-                lst = list(res.keys())
-                self.assertEqual(lst,results)
+    def test_aa_explicit_level_0(self):
+        index = self._makeOne(VALUES)
+        expected = [1,2,3,4,5,6,7,8,9]
 
-        for comp,level,results in tests:
-            for path in [comp,"/"+comp,"/"+comp+"/"]:
-                res = self._index.apply({"query":( (path,level),)})
-                lst = list(res.keys())
-                self.assertEqual(lst,results)
+        results = list(index.apply({'query': 'aa', 'level': 0}).keys())
+        self.assertEqual(results, expected)
 
-    def testComplexOrTests(self):
+        results = list(index.apply({'query': [('aa', 0)]}).keys())
+        self.assertEqual(results, expected)
 
-        self._populateIndex()
-        tests = [
-            (['aa','bb'],1,[1,2,3,4,5,6,10,11,12,13,14,15]),
-            (['aa','bb','xx'],1,[1,2,3,4,5,6,10,11,12,13,14,15]),
-            ([('cc',1),('cc',2)],0,[3,6,7,8,9,12,15,16,17,18]),
-        ]
+    def test_aa_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [1,2,3,10,11,12]
 
-        for lst ,level,results in tests:
-            
-            res = self._index.apply({"query":lst,"level":level,"operator":"or"})
-            lst = list(res.keys())
-            self.assertEqual(lst,results)
+        results = list(index.apply({'query': 'aa', 'level': 1}).keys())
+        self.assertEqual(results, expected)
 
-    def testComplexANDTests(self):
+        results = list(index.apply({'query': [('aa', 1)]}).keys())
+        self.assertEqual(results, expected)
 
-        self._populateIndex()
-        tests = [
-            (['aa','bb'],1,[]),
-            ([('aa',0),('bb',1)],0,[4,5,6]),
-            ([('aa',0),('cc',2)],0,[3,6,9]),
-        ]
+    def test_bb_level_0(self):
+        index = self._makeOne(VALUES)
+        expected = [10,11,12,13,14,15,16,17,18]
 
-        for lst ,level,results in tests:
-            res = self._index.apply(
-                {"query":lst,"level":level,"operator":"and"})
-            lst = list(res.keys())
-            self.assertEqual(lst,results)
+        results = list(index.apply({'query': 'bb', 'level': 0}).keys())
+        self.assertEqual(results, expected)
 
-    def testQueryPathReturnedInResult(self):
+        results = list(index.apply({'query': [('bb', 0)]}).keys())
+        self.assertEqual(results, expected)
 
-        index = self._index
+    def test_bb_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [4,5,6,13,14,15]
+
+        results = list(index.apply({'query': 'bb', 'level': 1}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('bb', 1)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_bb_cc_level_0(self):
+        index = self._makeOne(VALUES)
+        expected = [16,17,18]
+
+        results = list(index.apply({'query': 'bb/cc', 'level': 0}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('bb/cc', 0)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_bb_cc_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [6,15]
+
+        results = list(index.apply({'query': 'bb/cc', 'level': 1}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('bb/cc', 1)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_bb_aa_level_0(self):
+        index = self._makeOne(VALUES)
+        expected = [10,11,12]
+
+        results = list(index.apply({'query': 'bb/aa', 'level': 0}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('bb/aa', 0)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_bb_aa_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [4,13]
+
+        results = list(index.apply({'query': 'bb/aa', 'level': 1}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('bb/aa', 1)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_aa_cc_level_neg1(self):
+        index = self._makeOne(VALUES)
+        expected = [3,7,8,9,12]
+
+        results = list(index.apply({'query': 'aa/cc', 'level': -1}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('aa/cc', -1)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_bb_bb_level_neg1(self):
+        index = self._makeOne(VALUES)
+        expected = [5,13,14,15]
+
+        results = list(index.apply({'query': 'bb/bb', 'level': -1}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('bb/bb', -1)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_18_html_level_3(self):
+        index = self._makeOne(VALUES)
+        expected = [18]
+
+        results = list(index.apply({'query': '18.html', 'level': 3}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('18.html', 3)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_18_html_level_neg1(self):
+        index = self._makeOne(VALUES)
+        expected = [18]
+
+        results = list(index.apply({'query': '18.html', 'level': -1}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('18.html', -1)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_cc_18_html_level_2(self):
+        index = self._makeOne(VALUES)
+        expected = [18]
+
+        results = list(index.apply({'query': 'cc/18.html', 'level': 2}).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('cc/18.html', 2)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_cc_18_html_level_neg1(self):
+        index = self._makeOne(VALUES)
+        expected = [18]
+
+        results = list(index.apply({'query': 'cc/18.html', 'level': -1}
+                                  ).keys())
+        self.assertEqual(results, expected)
+
+        results = list(index.apply({'query': [('cc/18.html', -1)]}).keys())
+        self.assertEqual(results, expected)
+
+    def test_aa_bb_implicit_OR_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [1,2,3,4,5,6,10,11,12,13,14,15]
+        results = list(index.apply({'query': ['aa', 'bb'],
+                                    'level': 1,
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_aa_bb_explicit_OR_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [1,2,3,4,5,6,10,11,12,13,14,15]
+        results = list(index.apply({'query': ['aa', 'bb'],
+                                    'level': 1,
+                                    'operator': 'or',
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_aa_bb_xx_implicit_OR_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [1,2,3,4,5,6,10,11,12,13,14,15]
+        results = list(index.apply({'query': ['aa', 'bb', 'xx'],
+                                    'level': 1,
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_aa_bb_xx_explicit_OR_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [1,2,3,4,5,6,10,11,12,13,14,15]
+        results = list(index.apply({'query': ['aa', 'bb', 'xx'],
+                                    'level': 1,
+                                    'operator': 'or',
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_cc_level_1_cc_level_2_implicit_OR(self):
+        index = self._makeOne(VALUES)
+        expected = [3,6,7,8,9,12,15,16,17,18]
+        results = list(index.apply({'query': [('cc', 1), ('cc', 2)],
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_cc_level_1_cc_level_2_explicit_OR(self):
+        index = self._makeOne(VALUES)
+        expected = [3,6,7,8,9,12,15,16,17,18]
+        results = list(index.apply({'query': [('cc', 1), ('cc', 2)],
+                                    'operator': 'or',
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_aa_AND_bb_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = []
+        results = list(index.apply({'query': ['aa', 'bb'],
+                                    'level': 1,
+                                    'operator': 'and',
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_aa_level_0_AND_bb_level_1(self):
+        index = self._makeOne(VALUES)
+        expected = [4, 5, 6]
+        results = list(index.apply({'query': [('aa', 0), ('bb', 1)],
+                                    'operator': 'and',
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_aa_level_0_AND_cc_level_2(self):
+        index = self._makeOne(VALUES)
+        expected = [3, 6, 9]
+        results = list(index.apply({'query': [('aa', 0), ('cc', 2)],
+                                    'operator': 'and',
+                                   }).keys())
+        self.assertEqual(results, expected)
+
+    def test_query_path_included_with_childrent(self):
+        index = self._makeOne()
         index.index_doc(1, Dummy("/ff"))
         index.index_doc(2, Dummy("/ff/gg"))
         index.index_doc(3, Dummy("/ff/gg/3.html"))
         index.index_doc(4, Dummy("/ff/gg/4.html"))
-        res = index.apply({'query':'/ff/gg'})
-        lst = list(res.keys())
-        self.assertEqual(lst, [2, 3, 4])
+        result = list(index.apply({'query':'/ff/gg'}).keys())
+        self.assertEqual(result, [2, 3, 4])
 
 class Dummy:
 
@@ -190,4 +332,23 @@ class Dummy:
 
     __repr__ = __str__
 
-
+VALUES = {
+    1 : Dummy("/aa/aa/aa/1.html"),
+    2 : Dummy("/aa/aa/bb/2.html"),
+    3 : Dummy("/aa/aa/cc/3.html"),
+    4 : Dummy("/aa/bb/aa/4.html"),
+    5 : Dummy("/aa/bb/bb/5.html"),
+    6 : Dummy("/aa/bb/cc/6.html"),
+    7 : Dummy("/aa/cc/aa/7.html"),
+    8 : Dummy("/aa/cc/bb/8.html"),
+    9 : Dummy("/aa/cc/cc/9.html"),
+    10 : Dummy("/bb/aa/aa/10.html"),
+    11 : Dummy("/bb/aa/bb/11.html"),
+    12 : Dummy("/bb/aa/cc/12.html"),
+    13 : Dummy("/bb/bb/aa/13.html"),
+    14 : Dummy("/bb/bb/bb/14.html"),
+    15 : Dummy("/bb/bb/cc/15.html"),
+    16 : Dummy("/bb/cc/aa/16.html"),
+    17 : Dummy("/bb/cc/bb/17.html"),
+    18 : Dummy("/bb/cc/cc/18.html")
+}
