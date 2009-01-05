b925d390879ab214ee77f4fc63cc394b2ee23ad2
Chris McDonough <chrism@plope.com>
2009-01-05 03:24:02 +0000

    Various improvements.

--- repoze/catalog/benchmark/sortbench.py
+++ repoze/catalog/benchmark/sortbench.py
@@ -15,39 +15,65 @@ from pychart import text_box
 
 from BTrees.IFBTree import IFSet
 from repoze.catalog.indexes.field import fwscan_wins
+from repoze.catalog.indexes.field import nbest_ascending_wins
 
 theme.get_options()
 theme.use_color = True
 theme.scale_factor = 2
 
+# db keys:
+# 64
+# 512
+# 1024
+# 2048
+# 4096
+# 8192
+# 16384
+# 32768
+# 65536
+
 class FieldIndexForwardSort:
     """ Benchmark and compare the field index forward sort algorithms """
 
-    def __init__(self, limitbase=2, rlenbase=2, dbfn='index.db'):
+    def __init__(self, limitbase=2, rlenbase=2, dbfn='sort.db', dbkey='65536'):
         self.limitbase = limitbase
         self.rlenbase = rlenbase
-        self.index = get_index(dbfn)
+        self.dbfn = dbfn
+        self.dbkey = dbkey
+        self.index = self.get_index()
         self.numdocs = self.index._num_docs.value
+        self.dbkey = dbkey
         # the set of rlens and limits are series generated via
         # exponents to the power of the base base, e.g.  [4, 16, 64,
         # 256, 1024, 4096, 16384, 65535 ] if numdocs = 65708 and the
         # base is 4
-        self.rlens = series(self.numdocs, self.rlenbase)
-        self.limits = series(self.numdocs, self.limitbase)
+        self.rlens = series(self.numdocs+1, self.rlenbase)
+        self.limits = series(self.numdocs+1, self.limitbase)
 
         self.sorts = (
             ('nbest', self.index.nbest_ascending),
             ('fwscan', self.index.scan_forward),
             ('timsort', self.index.timsort_ascending)
             )
+ 
+    def get_index(self):
+        if not os.path.exists(self.dbfn):
+            raise NotImplementedError # XXX create index-creation code
+        from ZODB.FileStorage.FileStorage import FileStorage
+        from ZODB.DB import DB
+        s = FileStorage(self.dbfn)
+        db = DB(s, cache_size=300000)
+        c = db.open()
+        root = c.root()
+        return root[self.dbkey]
 
     def __call__(self):
-        if not os.path.exists('main.pck'):
+        if not os.path.exists('%s.pck' % self.dbkey):
             self.bench()
         self.chart()
 
     def bench(self):
-        tf = open('main.txt', 'w')
+        tf = open('%s.txt' % self.dbkey, 'w')
 
         def output(msg):
             tf.write(msg + '\n')
@@ -81,15 +107,15 @@ class FieldIndexForwardSort:
                     if control[:limit] != result:
                         raise AssertionError((control[:limit], result))
                     data.append(t)
-                    output('%0.5f %s at limit %s' % (t, name, limit))
+                    output('%0.6f %s at limit %s' % (t, name, limit))
 
             main.append({'rlen':rlen, 'capture':capture})
 
-        cPickle.dump(main, open('main.pck', 'w'))
+        cPickle.dump(main, open('%s.pck' % self.dbkey, 'w'))
 
     def chart(self):
 
-        self.main = cPickle.load(open('main.pck'))
+        self.main = cPickle.load(open('%s.pck' % self.dbkey))
 
         for chartable in self.main:
             self.detailchart(chartable)
@@ -143,7 +169,7 @@ class FieldIndexForwardSort:
             ar.add_plot(
                 line_plot.T(label="%s" % sortname, data=linedata)
                 )
-        fd = open('detail-%s.pdf' % chartable['rlen'], 'w')
+        fd = open('detail-%s-%s.pdf' % (self.dbkey, chartable['rlen']), 'w')
         can = canvas.init(fd, 'pdf')
         ar.draw(can)
         tb.draw(can)
@@ -151,6 +177,8 @@ class FieldIndexForwardSort:
 
     def comparisonchart(self, sortname1, sortname2):
         linedata = []
+        test_total = 0
+        test_wrong = 0
 
         for rlendata in self.main:
             rlen = rlendata['rlen']
@@ -161,7 +189,10 @@ class FieldIndexForwardSort:
             cutoff = None
             wins = []
 
-            fwtest = sortname1 == 'fwscan' and sortname2 in ('nbest', 'timsort')
+            #test = sortname1 == 'fwscan' and sortname2 in ('nbest', 'timsort')
+            #test_fn = fwscan_wins
+            test = sortname1 == 'nbest' and sortname2 == 'timsort'
+            test_fn = nbest_ascending_wins
 
             for x in xrange(0, min(len(values1), len(values2))):
                 t1 = values1[x]
@@ -172,18 +203,39 @@ class FieldIndexForwardSort:
                 if won:
                     wins.append(limit)
 
-                if fwtest:
-                    curvewin = fwscan_wins(limit, rlen, self.numdocs)
+                wrongmsg = "wrong %s? rlen %s, limit %s (%0.5f > %0.5f)%s"
+
+                if test:
+                    test_total += 1
+                    curvewin = test_fn(limit, rlen, self.numdocs)
                     if won and (not curvewin):
-                        print "wrong curvelose?", rlen, limit, t1, t2
-                    if (not won) and curvewin:
-                        print "wrong curvewin?", rlen, limit, t1, t2
+                        extra = ''
+                        if (t1 /  t2) < .80: # more than 20% difference
+                            extra = " * (%0.2f)" % (t1/t2)
+                        print wrongmsg % ('curvelose', rlen, limit, t2, t1,
+                                          extra)
+                        test_wrong +=1
+                    elif (not won) and curvewin:
+                        extra = ''
+                        if (t2 /  t1) < .80: # more than 20% difference
+                            extra = " * (%0.2f)" % (t2/t1)
+                        print wrongmsg % ('curvewin', rlen, limit, t1, t2,
+                                          extra)
+                        test_wrong +=1
 
             for limit in wins:
                 limitratio = limit / float(self.numdocs)
                 linedata.append((doc_ratio, limitratio))
 
-        comparename = 'compare-%s-beats-%s' % (sortname1, sortname2)
+        if test:
+            if test_total:
+                test_right = test_total - test_wrong
+                test_percent = test_right / float(test_total)
+                print "test percentage %0.2f: (%s wrong out of %s)" % (
+                    test_percent, test_wrong, test_total)
+
+        comparename = 'compare-%s-%s-beats-%s' % (self.dbkey,
+                                                  sortname1, sortname2)
 
         xaxis=axis.X(label='Doc Ratio (rlen//numdocs)',
                      tic_interval=.1,
@@ -215,7 +267,7 @@ class FieldIndexForwardSort:
 
 def timer(fn, *args, **kw):
     times = []
-    for x in xrange(3):
+    for x in xrange(7):
         start = time.time()
         result = fn(*args, **kw)
         if not hasattr(result, '__len__'):
@@ -223,17 +275,6 @@ def timer(fn, *args, **kw):
         end = time.time()
         times.append(end-start)
     return median(times), result
-
-def get_index(fn):
-    if not os.path.exists(fn):
-        raise NotImplementedError # XXX create index-creation code
-    from ZODB.FileStorage.FileStorage import FileStorage
-    from ZODB.DB import DB
-    s = FileStorage(fn)
-    db = DB(s, cache_size=300000)
-    c = db.open()
-    root = c.root()
-    return root['index']
 
 def isect(seq1, seq2):
     res = []                     # start empty
