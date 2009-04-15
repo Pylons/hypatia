58fa2600216a243f0dbca2a260bd08655593929b
Chris McDonough <chrism@plope.com>
2009-04-15 07:30:43 +0000

    - Add metadata-related APIs to   ``repoze.catalog.document.DocumentMap``: ``add_metadata``,   ``remove_metadata``, ``get_metadata``.  "Metadata" is a freeform set   of key/value pairs related to a docid.  See the API documentation   for more information.

--- repoze/catalog/document.py
+++ repoze/catalog/document.py
@@ -3,6 +3,7 @@ import random
 from persistent import Persistent
 from BTrees.IOBTree import IOBTree
 from BTrees.OIBTree import OIBTree
+from BTrees.OOBTree import OOBTree
 
 import BTrees
 
@@ -15,13 +16,15 @@ class DocumentMap(Persistent):
     _v_nextid = None
     family = BTrees.family32
     _randrange = random.randrange
+    docid_to_metadata = None # latch for b/c
 
     def __init__(self):
         self.docid_to_address = IOBTree()
         self.address_to_docid = OIBTree()
+        self.docid_to_metadata = IOBTree()
 
     def add(self, address, docid=_marker):
-        """ Add a new document to the docment map.  ``address`` is a
+        """ Add a new document to the document map.  ``address`` is a
         string or other hashable object which represents a token known
         by the application.  This method returns an integer document
         id.  An optional ``docid`` argument allows you to add a a
@@ -33,26 +36,97 @@ class DocumentMap(Persistent):
         self.address_to_docid[address] = docid
         return docid
 
+    def _check_metadata(self):
+        # backwards compatibility
+        if self.docid_to_metadata is None:
+            self.docid_to_metadata = IOBTree()
+
+    def add_metadata(self, docid, data):
+        """
+        Add metadata related to docid.  If ``docid`` doesn't
+        relate to an address in in the document map, a KeyError will
+        be raised.
+
+        For each key/value pair in ``data`` (which should be a
+        mapping, such as a dictionary), insert a metadata key/value
+        pair into the metadata BTree.
+
+        When metadata values are added, any existing values related to
+        the keys are overwritten, but existing values that don't
+        relate to the keys are left unchanged.
+        """
+        if not docid in self.docid_to_address:
+            raise KeyError(docid)
+        self._check_metadata()
+        meta = self.docid_to_metadata.setdefault(docid, OOBTree())
+        for k in data:
+            meta[k] = data[k]
+        if not meta:
+            del self.docid_to_metadata[docid]
+
+    def remove_metadata(self, docid, *keys):
+        """ Remove metadata related to docid.  If the docid doesn't
+        exist in the metadata map, a KeyError will be raised.
+
+        For each key in ``keys``, remove the metadata value for the
+        docid related to that key.  If any entry related to a key does
+        not exist, no error will be raised.
+
+        If no keys are specified, all metadata related to the docid is
+        removed.
+        """
+        self._check_metadata()
+        if keys:
+            meta = self.docid_to_metadata.get(docid, None)
+            if meta is None:
+                raise KeyError(docid)
+            for k in keys:
+                if k in meta:
+                    del meta[k]
+            if not meta:
+                del self.docid_to_metadata[docid]
+        else:
+            if not (docid in self.docid_to_metadata):
+                raise KeyError(docid)
+            del self.docid_to_metadata[docid]
+
+    def get_metadata(self, docid):
+        """
+        Return the metadata BTree related to ``docid``.
+
+        If metadata does not exist for ``docid``, a KeyError is raised.
+        """
+        if self.docid_to_metadata is None:
+            raise KeyError(docid)
+        meta = self.docid_to_metadata[docid]
+        return meta
+
     def remove_docid(self, docid):
         """ Remove a document from the document map using an integer
         document id.  If the document id doesn't actually exist in the
         document map, return False.  Otherwise, return True."""
+        self._check_metadata()
         address = self.docid_to_address.get(docid)
         if address is None:
             return False
         del self.docid_to_address[docid]
         del self.address_to_docid[address]
+        if docid in self.docid_to_metadata:
+            del self.docid_to_metadata[docid]
         return True
 
     def remove_address(self, address):
         """ Remove a document from the document map using an address.
         If the address doesn't actually exist in the document map,
         return False.  Otherwise, return True."""
+        self._check_metadata()
         docid = self.address_to_docid.get(address)
         if docid is None:
             return False
         del self.docid_to_address[docid]
         del self.address_to_docid[address]
+        if docid in self.docid_to_metadata:
+            del self.docid_to_metadata[docid]
         return True
 
     def docid_for_address(self, address):
--- repoze/catalog/tests/test_document.py
+++ repoze/catalog/tests/test_document.py
@@ -19,13 +19,70 @@ class TestDocumentMap(unittest.TestCase):
         self.assertEqual(map.address_to_docid['/address3'], address2id)
         self.assertEqual(newaddress2id, address2id)
 
+    def test_add_metadata_not_in_addressmap(self):
+        map = self._makeOne()
+        self.assertRaises(KeyError, map.add_metadata, 1, {'a':'1'})
+
+    def test_add_metadata_in_addressmap(self):
+        map = self._makeOne()
+        docid = map.add('/address1')
+        map.add_metadata(docid, {'a':1, 'b':2})
+        self.assertEqual(map.docid_to_metadata[docid]['a'], 1)
+        self.assertEqual(map.docid_to_metadata[docid]['b'], 2)
+
+    def test_add_metadata_no_values(self):
+        map = self._makeOne()
+        docid = map.add('/address1')
+        map.add_metadata(docid, {})
+        self.failIf(docid in map.docid_to_metadata)
+       
+    def test_remove_metadata_nokeys_nodocid(self):
+        map = self._makeOne()
+        self.assertRaises(KeyError, map.remove_metadata, 1)
+        
+    def test_remove_metadata_nokeys_docid(self):
+        map = self._makeOne()
+        docid = map.add('/address1')
+        map.docid_to_metadata[docid] = None
+        map.remove_metadata(docid)
+        self.failIf(docid in map.docid_to_metadata)
+
+    def test_remove_metadata_keys_nodocid(self):
+        map = self._makeOne()
+        self.assertRaises(KeyError, map.remove_metadata, 1, 'a')
+
+    def test_remove_metadata_keys_docid(self):
+        map = self._makeOne()
+        docid = map.add('/address1')
+        map.docid_to_metadata[docid] = {'a':1, 'b':2, 'c':3}
+        map.remove_metadata(docid, 'a', 'b')
+        self.assertEqual(map.docid_to_metadata[docid], {'c':3})
+        map.remove_metadata(docid, 'c')
+        self.failIf(docid in map.docid_to_metadata)
+
+    def test_get_metadata_no_mainbtree(self):
+        map = self._makeOne()
+        map.docid_to_metadata = None
+        self.assertRaises(KeyError, map.get_metadata, 1)
+
+    def test_get_metadata_not_in_map(self):
+        map = self._makeOne()
+        self.assertRaises(KeyError, map.get_metadata, 1)
+
+    def test_get_metadata_nokeys(self):
+        map = self._makeOne()
+        map.docid_to_metadata[1] = {'a':1, 'b':2}
+        self.assertEqual(dict(map.get_metadata(1)), {'a':1, 'b':2})
+
     def test_remove_docid_exists(self):
         map = self._makeOne()
         map.add('/address1', 1)
+        map.docid_to_metadata[1] = None
         result = map.remove_docid(1)
         self.assertEqual(result, True)
         self.assertEqual(map.docid_to_address.get(1), None)
         self.assertEqual(map.address_to_docid.get('/address1'), None)
+        self.failIf(1 in map.docid_to_metadata)
 
     def test_remove_docid_doesntexist(self):
         map = self._makeOne()
@@ -33,14 +90,16 @@ class TestDocumentMap(unittest.TestCase):
         self.assertEqual(result, False)
         self.assertEqual(map.docid_to_address.get(1), None)
         self.assertEqual(map.address_to_docid.get('/address1'), None)
-        
+
     def test_remove_address_exists(self):
         map = self._makeOne()
         map.add('/address1', 1)
+        map.docid_to_metadata[1] = None
         result = map.remove_address('/address1')
         self.assertEqual(result, True)
         self.assertEqual(map.docid_to_address.get(1), None)
         self.assertEqual(map.address_to_docid.get('/address1'), None)
+        self.failIf(1 in map.docid_to_metadata)
 
     def test_remove_address_doesntexist(self):
         map = self._makeOne()
@@ -71,4 +130,11 @@ class TestDocumentMap(unittest.TestCase):
         map._randrange = randrange
         map.add('/whatever', 0)
         self.assertEqual(map.new_docid(), 1)
+
+    def test_check_metadata(self):
+        map = self._makeOne()
+        map.docid_to_metadata = None
+        map._check_metadata()
+        self.failIf(map.docid_to_metadata is None)
+        
         
