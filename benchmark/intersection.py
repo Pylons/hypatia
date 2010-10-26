9099446c051237af152e538d71f2b8ef22fec194
Chris Rossi <chris@christophermrossi.com>
2010-10-26 03:19:58 +0000

    Do benchmarks with a bunch of different scenarios.

--- benchmark/intersection.py
+++ benchmark/intersection.py
@@ -15,7 +15,11 @@ Two algorithms:
 
 1) Perform lookup on i2, yielding r2, then take intersection of r1 and r2.
 
-Lookup is O(log(nk2, oob))
+Fudge factor is introduced to account for fact that forward lookup is done with
+string keys whereas reverse lookup and intersection are performed with integer
+keys which have less expensive comparisons.
+
+Lookup is O(fudge * log(nk2, oob))
 Intersection is O(max(n1, n2))
 Total cost: O(log(nk2, oob) + max(n1, n2))
 or taking the average for n2, since not yet known,
@@ -29,12 +33,7 @@ Total cost: O(n1 * log(nd2, iob))
 
 2) beats 1) when:
 
-  n1 * log(nd2, iob) < log(nk2, oob) + max(n1, nd2/nk2)
-
-n1   | nd2   | nk2   | n1 log(nd2) | log(nk2) + max(n1, nd2/nk2) | Who wins?
-----------------------------------------------------------------------------
-10   | 2^20  | 2^18  | 200         | 22                          | 1
-10   | 2^20  | 2^10  | 200         | 1034                        | 2
+  n1 * log(nd2, iob) < fudge * log(nk2, oob) + max(n1, nd2/nk2)
 
 So given a relationship between number of keys and number of docs in i2 and
 size of r1, we can decide which algorithm is better.
@@ -50,6 +49,7 @@ import glob
 import math
 import os
 import random
+import sys
 import time
 
 from repoze.catalog.catalog import ConnectionManager
@@ -95,22 +95,23 @@ class Intersection2(SetOp):
                 results.add(docid)
         return results
 
-def do_benchmark(fname, nd, nk1, nk2):
+def do_benchmark(fname, nd, nk1, nk2, out=sys.stdout):
     cumulative1 = 0.0
     cumulative2 = 0.0
 
-    print "Index 1:"
-    print "\t# docs: %d" % nd
-    print "\t# distinct keys: %d" % nk1
-    print "Index 2:"
-    print "\t# docs: %d" % nd
-    print "\t# distinct keys: %d" % nk2
-    print ""
+    print >>out, "Index 1:"
+    print >>out, "\t# docs: %d" % nd
+    print >>out, "\t# distinct keys: %d" % nk1
+    print >>out, "Index 2:"
+    print >>out, "\t# docs: %d" % nd
+    print >>out, "\t# distinct keys: %d" % nk2
+    print >>out, ""
 
+    FUDGE = 1.5
     oob = 250 # OOBTree DEFAULT_MAX_BTREE_SIZE
     iob = 500 # IOBTree DEFAULT_MAX_BTREE_SIZE
-    L_FWD_LOOKUP_COST = math.log(nk1, oob)
-    R_FWD_LOOKUP_COST = math.log(nk2, oob)
+    L_FWD_LOOKUP_COST = FUDGE * math.log(nk1, oob)
+    R_FWD_LOOKUP_COST = FUDGE * math.log(nk2, oob)
     L_REV_LOOKUP_COST = math.log(nd, iob)
     AVG_L_RESULT_SIZE = float(nd)/nk1
     AVG_R_RESULT_SIZE = float(nd)/nk2
@@ -122,17 +123,19 @@ def do_benchmark(fname, nd, nk1, nk2):
     # Total cost: O(n1 * log(nd2, iob))
     cost2 = L_FWD_LOOKUP_COST + (AVG_L_RESULT_SIZE * L_REV_LOOKUP_COST)
 
-    print 'Cost1: %0.2f' % cost1
-    print 'Cost2: %0.2f' % cost2
-    print
-    print "Prediction:"
+    print >>out, 'Cost1: %0.2f' % cost1
+    print >>out, 'Cost2: %0.2f' % cost2
+    print >>out
+    print >>out, "Prediction:"
     if cost1 > cost2:
-        print "Algorithm 2 %0.2f times faster than Algorithm 1" % (cost1/cost2)
+        print >>out, "Algorithm 2 %0.2f times faster than Algorithm 1" % (
+            cost1/cost2)
     else:
-        print "Algorithm 1 %0.2f times faster than Algorithm 2" % (cost2/cost1)
+        print >>out, "Algorithm 1 %0.2f times faster than Algorithm 2" % (
+            cost2/cost1)
 
-    print ""
-    print "Setting up indexes..."
+    print >>out, ""
+    print >>out, "Setting up indexes..."
     for fn in glob.glob(fname + "*"):
         os.remove(fn)
 
@@ -144,49 +147,81 @@ def do_benchmark(fname, nd, nk1, nk2):
     catalog['two'] = CatalogFieldIndex('two')
 
     class Document(object):
-        def __init__(self):
-            self.one = str(random.randrange(nk1))
-            self.two = str(random.randrange(nk2))
+        def __init__(self, docid):
+            self.one = str(docid % nk1)
+            self.two = str(docid % nk2)
 
     for docid in xrange(nd):
-        catalog.index_doc(docid, Document())
+        catalog.index_doc(docid, Document(docid))
     manager.commit()
     manager.close()
 
-    print "Running %d queries for each algorithm..." % (nk1 * nk2)
+    N_QUERIES = 1000
+    print >>out, "Running %d queries for each algorithm..." % N_QUERIES
     catalog = factory(manager)
-    for key1 in xrange(nk1):
-        for key2 in xrange(nk2):
-            query1 = Intersection1(Eq('one', str(key1)), Eq('two', str(key2)))
-            query2 = Intersection2(Eq('one', str(key1)), Eq('two', str(key2)))
+    for _ in xrange(1000):
+        key1 = random.randrange(nk1)
+        key2 = random.randrange(nk2)
+        query1 = Intersection1(Eq('one', str(key1)), Eq('two', str(key2)))
+        query2 = Intersection2(Eq('one', str(key1)), Eq('two', str(key2)))
 
-            start = time.time()
-            result1 = query1.apply(catalog)
-            cumulative1 += time.time() - start
+        start = time.time()
+        result1 = query1.apply(catalog)
+        cumulative1 += time.time() - start
 
-            start = time.time()
-            result2 = query2.apply(catalog)
-            cumulative2 += time.time() - start
+        start = time.time()
+        result2 = query2.apply(catalog)
+        cumulative2 += time.time() - start
 
-            s1 = sorted(list(result1))
-            s2 = sorted(list(result2))
+        s1 = sorted(list(result1))
+        s2 = sorted(list(result2))
 
-            assert s1==s2, (s1, s2)
+        assert s1==s2, (s1, s2)
 
     manager.close()
     for fn in glob.glob(fname + "*"):
         os.remove(fn)
 
-    print ""
-    print "Result:"
-    print "Time for algorithm1: %0.3f s" % cumulative1
-    print "Time for algorithm2: %0.3f s" % cumulative2
+    print >>out, ""
+    print >>out, "Result:"
+    print >>out, "Time for algorithm1: %0.3f s" % cumulative1
+    print >>out, "Time for algorithm2: %0.3f s" % cumulative2
     if cumulative1 > cumulative2:
-        print "Algorithm 2 %0.2f times faster than Algorithm 1" % (
+        print >>out, "Algorithm 2 %0.2f times faster than Algorithm 1" % (
             cumulative1/cumulative2)
     else:
-        print "Algorithm 1 %0.2f times faster than Algorithm 2" % (
+        print >>out, "Algorithm 1 %0.2f times faster than Algorithm 2" % (
             cumulative2/cumulative1)
+    return cost1 / cost2, cumulative1 / cumulative2
+
+class Null(object):
+    def write(self, s):
+        pass
+
+def _range_order_of_magnitude(n):
+    # Iterate over (at most) 3 orders of magnitude
+    n_magnitude = math.ceil(math.log10(n))
+    lowest_magnitude = max(0, n_magnitude - 3)
+    for magnitude in xrange(lowest_magnitude, n_magnitude):
+        for i in xrange(1,10):
+            value = i * 10**magnitude
+            if value >= n:
+                break
+            yield value
+
+def do_benchmarks(fname):
+    null = Null()
+    print "Cost of algorithm 1 / Cost of algorithm 2"
+    print "N Docs | N Keys 1 | N Keys 2 | Predicted | Actual | Correct"
+    for nd in  [100, 1000, 10000, 100000, 1000000]:
+        for nk1 in _range_order_of_magnitude(nd / 2):
+            for nk2 in _range_order_of_magnitude(nd):
+                predicted, actual = do_benchmark(fname, nd, nk1, nk2, out=null)
+                correct = ((predicted > 1 and actual > 1) or
+                           (predicted < 1 and actual < 1))
+                print "%6d | %8d | %8d | %9.2f | %6.2f | %s" % (
+                    nd, nk1, nk2,  predicted, actual, correct)
+                sys.stdout.flush()
 
 # profile (unused right now)
 def profile(cmd, globals, locals, sort_order, callers):
@@ -211,4 +246,5 @@ def profile(cmd, globals, locals, sort_order, callers):
         os.remove(fn)
 
 if __name__ == '__main__':
-    do_benchmark('benchmark.db', 10000, 1000, 1000)
+    #do_benchmark('benchmark.db', 10000, 1000, 1000)
+    do_benchmarks('/dev/shm/benchmark.db')
