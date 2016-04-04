974962ba3ae103ebb12b4d570c6c3240e43c4971
Tres Seaver <tseaver@palladion.com>
2016-04-04 10:09:34 -0400

    Rename CHANGES.txt -> CHANGES.rst for better Github integration.

--- setup.py
+++ setup.py
@@ -54,8 +54,12 @@ class optional_build_ext(build_ext):
 
 try:
     here = os.path.abspath(os.path.dirname(__file__))
-    README = open(os.path.join(here, 'README.rst')).read()
-    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
+
+    with open(os.path.join(here, 'CHANGES.rst')) as f:
+        CHANGES = f.read()
 except IOError:
     README = CHANGES = ''
 
